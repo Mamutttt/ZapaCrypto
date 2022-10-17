65dada2b0388e1e641cfe5a180816af6063127de
fraanovich <franciscogonzalezkorin@gmail.com>
2022-10-17 20:37:27 -0300

    Actualizo archivo con funcionalidades de SyL

--- SyL.py
+++ SyL.py
@@ -4,7 +4,7 @@ def mostrarArt():
     bdd = BDD()
     conexion = bdd.conexion_bdd()
     cursor = conexion.cursor()
-    cursor.execute("SELECT * FROM Articulo")
+    cursor.execute("SELECT <getCampo> FROM Articulo")
     respuesta = cursor.fetchall()
     for x in respuesta:
         print(x)
@@ -14,6 +14,48 @@ def aStockArticulo():
     conexion = bdd.conexion_bdd()
     cursor = conexion.cursor()
     cursor.execute("INSERT INTO Articulo VALUES (<getIdArt>, <getIdProd>, <getTalle>, <getColor>)")
+    cursor.execute("SELECT * FROM Articulo")
+    respuesta = cursor.fetchall()
+    for x in respuesta:
+        print(x)
+
+mostrarArt()
+
+def bStockArticulo():
+    bdd = BDD()
+    conexion = bdd.conexion_bdd()
+    cursor = conexion.cursor()
+    cursor.execute("DELETE FROM Articulo WHERE id_articulo=<getIdArt>")
+    cursor.execute("SELECT * FROM Articulo")
+    respuesta = cursor.fetchall()
+    for x in respuesta:
+        print(x)
+
+def mStockArticulo():
+    bdd = BDD()
+    conexion = bdd.conexion_bdd()
+    cursor = conexion.cursor()
+    cursor.execute("UPDATE Articulo SET <getCampo>=<getValor> WHERE id_articulo=<getArtId>")
+    cursor.execute("SELECT * FROM Articulo")
+    respuesta = cursor.fetchall()
+    for x in respuesta:
+        print(x)
+
+def aStockProducto():
+    bdd = BDD()
+    conexion = bdd.conexion_bdd()
+    cursor = conexion.cursor()
+    cursor.execute("INSERT INTO Producto VALUES (<getIdProd>, <getModel>, <getDesc>, <getPrecio>)")
+    cursor.execute("SELECT * FROM Articulo")
+    respuesta = cursor.fetchall()
+    for x in respuesta:
+        print(x)
+
+def mostrarProd():
+    bdd = BDD()
+    conexion = bdd.conexion_bdd()
+    cursor = conexion.cursor()
+    cursor.execute("SELECT <getCampo> FROM Producto")
     respuesta = cursor.fetchall()
     for x in respuesta:
         print(x)

489616bb2e91ff0fc1b398cf3b0e6bae84adab0f
Daniel-Wonders <lautaroalloco@gmail.com>
2022-10-18 15:48:13 -0300

    Ahi termine el registro de la compra, stock:

---
+++ Proyecto_Final_Desarrollo_2022 - copia/Conexion_BDD_V2.py
@@ -0,0 +1,194 @@
+import mysql.connector
+
+class BDD:
+    #Definimos los datos de la conexión
+    direccion_servidor = 'aws-sa-east-1.connect.psdb.cloud'
+    nombre_bd = 'zapacryptodb'
+    usr_bd = 'avtrlpr7q9s8fg8kd3yp'
+    pwd_bd = 'pscale_pw_v2vCTEV5quiGIwG5wA8c0BKVHUvamHiNljdbnvReCQY'
+    
+    #Datos usuario
+    id_rol = 0
+    id_usuario = ""
+    usuario = ""
+    contraseña = ""
+    nombre = ""
+    apellido = ""
+    dni = ""
+    mail = ""
+    teléfono = ""
+    código_postal = ""
+          
+    
+    def conectar_bdd(self):   
+        #Establecemos la conexión con gestión de errores
+        try:
+            self.conexion = mysql.connector.connect(host= self.direccion_servidor,
+                                                database=self.nombre_bd,
+                                                user=self.usr_bd,
+                                                password=self.pwd_bd)
+            #Conexion exitosa
+            #Se recupera la información de la conexión
+            db_Info = self.conexion.get_server_info()
+            print("Connected to MySQL Server version ", db_Info)
+            
+            return self.conexion
+
+        except mysql.connector.Error as e:
+            #Falla en la conexión
+            print("Ocurrió un error al conectar a SQL Server: ", e)
+            
+
+    def validar_login(self):
+        #Consulta
+        # 0 - Error en la ejecución
+        # 1 - Usuario inexistente
+        # 2 - Usuario existente, contra incorrecta
+        # 3 - Usuario y contraseña correctos 
+        consulta = 1
+        print(self.contraseña)
+        try:
+            #Armamos la consulta
+            cursor = self.conexion.cursor()
+            sp = """SELECT
+			U.id_usuario
+			, U.contra
+		FROM
+			Usuario `U`
+		WHERE
+			U.usuario = '""" + self.usuario + "';"""
+	    
+            cursor.execute(sp)
+            contra = cursor.fetchall()
+           
+            #Validamos que haya encontrado un usuario 
+            if(len(contra)!=0):
+                consulta+=1
+                for linea in contra:
+                    #Validamos que la contraseña sea correcta y, si lo es,
+                    #carga el id del usuario (para recuperar su rol y demás)
+                    if(linea[1]==self.contraseña):
+                        consulta+=1
+                        self.id_usuario = linea[0] 
+                
+            cursor.close()
+
+        except Exception as e:
+            consulta = 0
+            
+        finally:
+            return consulta
+
+    def registrar_usuario(self):
+        mensaje = ""
+        try:
+            #Validamos que no exista el usuario
+            if(self.validar_login()>1):
+                mensaje = "El nombre de usuario dado ya existe. Por favor, elija otro."
+
+            #Validamos que no haya ocurrido un error en la validación
+            elif(self.validar_login()==0):
+                mensaje = "Lo sentimos, hubo un error inesperado en el sistema. Vuelva a intentarlo."
+
+            #Registramos al usuario
+            else:
+                cursor = conexion.cursor()
+                sp = """INSERT INTO
+                        Usuario(usuario
+                                , contra
+                                , nombre
+                                , apellido
+                                , dni
+                                , mail
+                                , telefono
+                                , codigo_postal
+                                )
+                        VALUES('""" + self.usuario + """'
+                                ,'""" + self.contraseña + """'
+                                ,'""" + self.nombre + """'
+                                ,'""" + self.apellido + """'
+                                ,'""" + str(self.dni) + """'
+                                ,'""" + self.mail + """'
+                                ,'""" + str(self.teléfono) + """'
+                                ,'""" + self.código_postal + """'
+                                );""" 
+                     
+                cursor.execute(sp)
+                self.conexion.commit()
+
+                cursor.close()
+                
+                mensaje = "Se registro al usuario correctamente."
+
+        except Exception as e:
+            mensaje = str(e)
+
+        finally:
+            print(mensaje)
+            return mensaje
+            
+                           
+    def recuperar_rol(self):
+        try:
+            #Armamos la consulta
+            cursor = self.conexion.cursor()
+            sp = """SELECT
+                        RUR.id_rol
+                    FROM
+                        Usuario `U`
+                        , rel_usuario_rol `RUR`
+                    WHERE
+                        U.id_usuario = RUR.id_usuario
+                    AND
+			U.id_usuario = '""" + str(self.id_usuario) + "';"""
+	    
+            cursor.execute(sp)
+            rol = cursor.fetchone()
+            
+            #Validamos que haya encontrado un usuario 
+            if(rol!=None):
+                for linea in rol:
+                    #Validamos que la contraseña sea correcta
+                    self.id_rol = linea             
+            else:
+                print("Usuario común, sin rol.")
+                
+            cursor.close()
+            
+        except Exception as e:
+            print(e)
+            
+    
+if __name__=='__main__':
+    obj_conexion = BDD()
+    conexion = obj_conexion.conectar_bdd()
+
+    #Listado de mensajes a imprimir según el resultado de la validación
+    mensajes = ["Se produjo un error en la validación."
+                ,"El usuario no existe."
+                ,"La contraseña es incorrecta."
+                ,"Ingreso correcto. Bienvenido al sistema."]
+    
+    #Datos de prueba que deberían ser recuperados con un txtnombrelabel.get()
+    obj_conexion.usuario = "caro2"
+    obj_conexion.contraseña = "1234567"
+
+    #Prueba de validación de de datos, con su correspondiente mensaje
+    posicion= obj_conexion.validar_login()
+    print(mensajes[posicion])
+
+    obj_conexion.nombre = "Carolina"
+    obj_conexion.apellido = "Linares"
+    obj_conexion.dni = "44939978"
+    obj_conexion.mail = "carolinares003@gmail.com"
+    obj_conexion.teléfono = "1127354645"
+    obj_conexion.código_postal = "1407"
+    
+
+    print(obj_conexion.registrar_usuario())
+    
+    obj_conexion.validar_login()
+    
+    obj_conexion.recuperar_rol()
+
+    
---
+++ Proyecto_Final_Desarrollo_2022 - copia/main2.py
@@ -0,0 +1,514 @@
+import easygui
+
+import tkinter
+from tkinter import *
+
+from tkinter import ttk
+from tkinter import messagebox 
+import tkinter.font as tkFont
+from PIL import ImageTk, Image
+
+import warnings
+
+from functools import partial
+
+from Conexion_BDD_V2 import BDD
+
+############ holis 2
+# command = lambda:[<nombre función1(), <nombre función2()]
+
+
+class Entorno_Grafico():
+    
+    def __init__(self):
+        #Pantalla de inicio
+        self.ventana = Tk()
+        self.ventana.geometry("900x600")
+        self.ventana.title("ZapaCrypto61")
+        self.ventana.iconbitmap("Fotos/Logo.ico")
+        
+        Fondo = tkinter.PhotoImage(file="Fotos/Fondo.png")
+        
+        background = Label(image=Fondo, text="Imagen S.O de fondo")
+        background.place(x=0, y=0, relwidth=1, relheight=1)
+
+        Logo = tkinter.PhotoImage(file=r"Fotos/Logo.PNG")
+        LogoIMG = tkinter.Label(self.ventana, image=Logo)
+        LogoIMG.place(x=350, y=100)
+        Label(text="Administra tu empresa").place(x=390, y=310)
+        
+        fontStyle = tkFont.Font(family="Bell Gothic Std Black", size=10)
+     
+        #Botones
+        imagen_boton = Image.open("imagen_boton_low.png")
+        imagen_ajustada = imagen_boton.resize((130,70), Image.ANTIALIAS)
+        self.imagen_final = ImageTk.PhotoImage(imagen_ajustada)
+        
+        btn_inicio = Button(self.ventana,
+                            image=self.imagen_final,
+                            bg = "#1c1f50", borderwidth=0,
+                            font=fontStyle, fg = "black",
+                            text = "Iniciar sesión", compound='center',
+                            command= self.CargarInicio)
+        
+        btn_inicio.place(x=300, y=350)
+        
+        btn_registro = Button(self.ventana,
+                              image=self.imagen_final,
+                              bg = "#1c1f50", borderwidth=0,
+                              font=fontStyle, fg = "black",
+                              text = "Registrarse", compound='center',
+                              command= self.CargarRegistro)
+        
+        btn_registro.place(x=470, y=350)
+
+        
+        SYL = PhotoImage(file=r"Fotos/StockYLogística.png")
+        IMG1 = Button(self.ventana, image=SYL, borderwidth=0, command=self.StockYLogística)
+        IMG1.place(x=150, y=150)
+        Label(text="Stock y Logística").place(x=150, y=260)
+
+        VYC = PhotoImage(file=r"Fotos/VentasYClientes.png")
+        IMG2 = Button(self.ventana, image=VYC, borderwidth=0, command=self.VentasYClientes)
+        IMG2.place(x=150, y=300)
+        Label(text="Ventas y Clientes").place(x=150, y=410)
+
+        CYP = PhotoImage(file=r"Fotos/ComprasYProveedores.png")
+        IMG3 = Button(self.ventana, image=CYP, borderwidth=0, command=self.ComprasYProveedores)
+        IMG3.place(x=650, y=150)
+        Label(text="Compras y Proveedores").place(x=650, y=260)
+
+        CONT = PhotoImage(file=r"Fotos/Contabilidad.png")
+        IMG4 = Button(self.ventana, image=CONT, borderwidth=0, command=self.Contabilidad)
+        IMG4.place(x=650, y=300)
+        Label(text="Contabilidad").place(x=650, y=410)
+
+
+        #Instancia de capa de archivos compartida
+        self.obj_conexion = BDD()
+        self.conexion = self.obj_conexion.conectar_bdd()
+
+        #Listado de mensajes a imprimir según el resultado de la validación
+        self.mensajes = ["Se produjo un error en la validación."
+                        ,"El usuario no existe."
+                        ,"La contraseña es incorrecta."
+                        ,"Ingreso correcto. Bienvenido al sistema."]
+        
+        
+        self.ventana.mainloop()
+        
+        
+    def Atrás(self, ventana_a_cerrar):
+        ventana_a_cerrar.destroy()
+        self.ventana.deiconify()
+
+
+#Sesión de usuario
+    def CargarInicio(self):
+        #Ventana de inicio
+        ventanaI = Toplevel()
+        ventanaI.geometry("300x225")
+        ventanaI.configure(bg="pink")
+        ventanaI.title("ZapaCrypto61 - Login")
+        
+        ventanaI.iconbitmap("Fotos/Logo.ico")
+
+        #Detalles de usuario
+        lbl_usuario = Label(ventanaI,
+                            bg = "pink", borderwidth=0,
+                            font=('Helvetica', 12),
+                            #font=fontStyle, fg = "black",
+                            text = "Usuario: ")
+        lbl_usuario.place(x=60, y=40)
+
+        self.txt_usuario = Text(ventanaI,
+                           font=('Century 12'),
+                           borderwidth=0)
+        self.txt_usuario.place(x=150, y=40, width=90, height=20)
+
+        
+        lbl_contra = Label(ventanaI,
+                           bg = "pink", borderwidth=0,
+                           font=('Helvetica', 12),
+                           text = "Contraseña: ")
+        lbl_contra.place(x=40, y=90)
+        
+        self.txt_contra = Text(ventanaI,
+                          font=('Century 12'),
+                          borderwidth=0)
+        self.txt_contra.place(x=150, y=90, width=100, height=20)
+
+
+        #Boton de validación
+        btn_inicio = Button(ventanaI,
+                            image= self.imagen_final,
+                            bg = "pink", borderwidth=0,
+                            font=('Helvetica', 10),
+                            text = "Iniciar sesión", compound='center',
+                            command= partial(self.validar_login, ventanaI))
+        
+        btn_inicio.place(x=80, y=130)
+
+    def validar_login(self, ventana):
+        #Recuperamos los datos del usuario
+        self.obj_conexion.usuario = self.txt_usuario.get("1.0","end-1c")
+        self.obj_conexion.contraseña = self.txt_contra.get("1.0","end-1c")
+
+        #El resultado del cotejo de datos en la bdd  
+        #nos indicará el resultado de la operación hecha
+        posicion = self.obj_conexion.validar_login()
+        easygui.msgbox(self.mensajes[posicion], title = "Login")
+
+        #Si la validación fue correcta, el usuario inicia su
+        #sesión, cargando por default la pantalla de Stock y Logística
+        if(posicion==3):
+            ventana.destroy()
+            self.obj_conexion.recuperar_rol()
+            self.StockYLogística()
+
+        
+    def CargarRegistro(self):
+        ventanaR = Tk()
+        
+        lbl_usuario = Label(ventanaR,
+                            bg = "pink", borderwidth=0,
+                            #font=fontStyle, fg = "black",
+                            text = "Usuario: ")
+        lbl_usuario.pack()
+
+        txt_usuario = Text(ventanaR,
+                           borderwidth=0)
+        txt_usuario.pack()
+
+
+#Stock y Logística
+    def StockYLogística(self):
+        SYL = Tk()
+        SYL.geometry("900x600")
+        SYL.configure(bg="pink")
+        SYL.title("Stock y Logística")
+
+        BSYL = tkinter.Button(SYL, text="Volver", command=lambda: [self.Atrás(SYL), ()])
+        BSYL.place(x=850, y=5)
+
+        ASYL = tkinter.Button(SYL, text="Stock Y \nLogística", height=5, width=12, command=lambda: [])
+        ASYL.place(x=30, y=100)
+
+        AVYC = tkinter.Button(SYL, text="Ventas Y \nClientes", height=5, width=12, command=lambda: [self.VentasYClientes(), SYL.withdraw()])
+        AVYC.place(x=30, y=200)
+
+        ACYP = tkinter.Button(SYL, text="Compras Y \nProveedores", height=5, width=12, command=lambda: [self.ComprasYProveedores(), SYL.withdraw()])
+        ACYP.place(x=30, y=300)
+
+        ACONT = tkinter.Button(SYL, text="Contabilidad", height=5, width=12, command=lambda: [self.Contabilidad(), SYL.withdraw()])
+        ACONT.place(x=30, y=400)
+
+        B1SYL = tkinter.Button(SYL, text="Funcionalidad 1  ", command=lambda: [self.Fun1SYL(),])
+        B1SYL.place(x=150, y=50)
+
+        B2SYL = tkinter.Button(SYL, text="Funcionalidad 2  ", command="")
+        B2SYL.place(x=150, y=75)
+
+        B3SYL = tkinter.Button(SYL, text="Funcionalidad 3  ", command="")
+        B3SYL.place(x=150, y=100)
+
+        B4SYL = tkinter.Button(SYL, text="Funcionalidad 4  ", command="")
+        B4SYL.place(x=150, y=125)
+
+        B5SYL = tkinter.Button(SYL, text="Funcionalidad 5  ", command="")
+        B5SYL.place(x=150, y=150)
+
+        B6SYL = tkinter.Button(SYL, text="Funcionalidad 6  ", command="")
+        B6SYL.place(x=150, y=175)
+
+        B7SYL = tkinter.Button(SYL, text="Funcionalidad 7  ", command="")
+        B7SYL.place(x=150, y=200)
+
+        B8SYL = tkinter.Button(SYL, text="Funcionalidad 8  ", command="")
+        B8SYL.place(x=150, y=225)
+
+        B9SYL = tkinter.Button(SYL, text="Funcionalidad 9  ", command="")
+        B9SYL.place(x=150, y=250)
+
+        B10SYL = tkinter.Button(SYL, text="Funcionalidad 10", command="")
+        B10SYL.place(x=150, y=275)
+
+        B11SYL = tkinter.Button(SYL, text="Funcionalidad 11", command="")
+        B11SYL.place(x=150, y=300)
+
+        B12SYL = tkinter.Button(SYL, text="Funcionalidad 12", command="")
+        B12SYL.place(x=150, y=325)
+
+        B13SYL = tkinter.Button(SYL, text="Funcionalidad 13", command="")
+        B13SYL.place(x=150, y=350)
+
+        B14SYL = tkinter.Button(SYL, text="Funcionalidad 14", command="")
+        B14SYL.place(x=150, y=375)
+
+        B15SYL = tkinter.Button(SYL, text="Funcionalidad 15", command="")
+        B15SYL.place(x=150, y=400)
+
+        B16SYL = tkinter.Button(SYL, text="Funcionalidad 16", command=lambda: [(), ()])
+        B16SYL.place(x=150, y=425)
+
+        self.ventana.withdraw()
+
+    def Fun1SYL(self):
+        F1SYL = Tk()
+        F1SYL.geometry("900x600")
+        F1SYL.title("Función 1")
+
+    def registrarCompra(self,art):
+
+
+
+        print("Se registro la compra"+art)
+        datos= list()
+        for i in range (len(art)):
+            if art[i]!=",":
+                datos.append(art[i])
+        print(datos)
+        easygui.msgbox("Se registró la compra exitosamente de los articulos: "+datos, title="ZapaCrypto")
+
+#Los datos que hay que pasar a stock, stock:
+
+
+    def mostrarArticulos(self):
+
+        self.ventanaArt=Tk()
+        self.ventanaArt.geometry("1000x700")
+        self.ventanaArt.configure(bg="pink")
+        Articulos=Listbox(self.ventanaArt,height=30,width=50)
+        Articulos.place(x=20,y=33)
+
+        BVYC = tkinter.Button(self.ventanaArt, text="Volver", command=lambda: [Atrás(self.ventanaArt)])
+        BVYC.place(x=950, y=5)
+
+        Labelart = Label(self.ventanaArt, text="ID", bg="PINK", font=("Helvetica", 14))
+        Labelart.place(x=15, y=5)
+        Labelart3 = Label(self.ventanaArt, text="Lista de articulos", bg="PINK", font=("Helvetica", 14))
+        Labelart3.place(x=40, y=5)
+        # Meter articulos en la lista
+        bdd = BDD()
+        conexion = bdd.conectar_bdd()
+        cursor = conexion.cursor()
+        cursor.execute("SELECT * FROM Articulo")
+        respuesta = cursor.fetchall()
+        for x in respuesta:
+            Articulos.insert(END, x)
+
+        Labelart2 = Label(self.ventanaArt, text="Eliga ID de articulos a comprar", bg="PINK", font=("Helvetica", 14))
+        Labelart2.place(x=350, y=40)
+        Labelart4 = Label(self.ventanaArt, text="(separado por comas)", bg="PINK", font=("Helvetica", 8))
+        Labelart4.place(x=616, y=46)
+
+        Entryvyc = Entry(self.ventanaArt, text="...", width=50)
+        Entryvyc.place(x=350, y=70)
+
+
+        botonCompra = Button(self.ventanaArt, text="Comprar", command=lambda:[self.registrarCompra(Entryvyc.get())], font=("Helvetica", 18))
+        botonCompra.place(x=430, y=110)
+
+
+        print ("Mostrar articulos")
+
+    def atencionAlCliente(self):
+        easygui.msgbox("Para comunicarse con atencion al cliente, comunicarse con: 11 2561-1025", title="Atencion al cliente")
+
+
+#Ventas y Clientes
+    def VentasYClientes(self):
+        VYC = Tk()
+        VYC.geometry("900x600")
+        VYC.configure(bg="pink")
+        VYC.title("Ventas y Clientes")
+
+        BVYC = tkinter.Button(VYC, text="Volver", command=lambda: [self.Atrás(VYC), ()])
+        BVYC.place(x=850, y=5)
+        ASYL = tkinter.Button(VYC, text="Stock Y \nLogística", height=5, width=12, command=lambda: [self.StockYLogística(), VYC.withdraw()])
+        ASYL.place(x=30, y=100)
+        AVYC = tkinter.Button(VYC, text="Ventas Y \nClientes", height=5, width=12, command=lambda: [])
+        AVYC.place(x=30, y=200)
+        ACYP = tkinter.Button(VYC, text="Compras Y \nProveedores", height=5, width=12, command=lambda: [self.ComprasYProveedores(), VYC.withdraw()])
+        ACYP.place(x=30, y=300)
+        ACONT = tkinter.Button(VYC, text="Contabilidad", height=5, width=12, command=lambda: [self.Contabilidad(), VYC.withdraw()])
+        ACONT.place(x=30, y=400)
+
+        B1VYC = tkinter.Button(VYC, text="Mostrar articulos ", font=("Helvetica", 36),
+                               command=lambda: [self.mostrarArticulos(), ])
+        B1VYC.place(x=300, y=200)
+
+        B2VYC = tkinter.Button(VYC, text="Atencion al cliente  ", font=("Helvetica", 13),
+                               command=lambda: [self.atencionAlCliente()])
+        B2VYC.place(x=700, y=550)
+
+        #B16VYC = tkinter.Button(VYC, text="Funcionalidad 16", command=lambda: [(), ()])
+        #B16VYC.place(x=150, y=425)
+
+        self.ventana.withdraw()
+        
+
+#Compras y Proveedores
+    def ComprasYProveedores(self):
+        CYP = Tk()
+        CYP.geometry("900x600")
+        CYP.title("Compras y Proveedores")
+
+        BCYP = tkinter.Button(CYP, text="Volver", command=lambda: [self.Atrás(CYP), ()])
+        BCYP.place(x=850, y=5)
+
+        ASYL = tkinter.Button(CYP, text="Stock Y \nLogística", height=5, width=12, command=lambda: [self.StockYLogística(), CYP.withdraw()])
+        ASYL.place(x=30, y=100)
+
+        AVYC = tkinter.Button(CYP, text="Ventas Y \nClientes", height=5, width=12, command=lambda: [self.VentasYClientes(), CYP.withdraw()])
+        AVYC.place(x=30, y=200)
+
+        ACYP = tkinter.Button(CYP, text="Compras Y \nProveedores", height=5, width=12, command=lambda: [])
+        ACYP.place(x=30, y=300)
+
+        ACONT = tkinter.Button(CYP, text="Contabilidad", height=5, width=12, command=lambda: [self.Contabilidad(), CYP.withdraw()])
+        ACONT.place(x=30, y=400)
+
+        B1CYP = tkinter.Button(CYP, text="Funcionalidad 1  ", command=lambda: [self.Proveedores(),])
+        B1CYP.place(x=150, y=50)
+
+        B2CYP = tkinter.Button(CYP, text="Funcionalidad 2  ", command="")
+        B2CYP.place(x=150, y=75)
+
+        B3CYP = tkinter.Button(CYP, text="Funcionalidad 3  ", command="")
+        B3CYP.place(x=150, y=100)
+
+        B4CYP = tkinter.Button(CYP, text="Funcionalidad 4  ", command="")
+        B4CYP.place(x=150, y=125)
+
+        B5CYP = tkinter.Button(CYP, text="Funcionalidad 5  ", command="")
+        B5CYP.place(x=150, y=150)
+
+        B6CYP = tkinter.Button(CYP, text="Funcionalidad 6  ", command="")
+        B6CYP.place(x=150, y=175)
+
+        B7CYP = tkinter.Button(CYP, text="Funcionalidad 7  ", command="")
+        B7CYP.place(x=150, y=200)
+
+        B8CYP = tkinter.Button(CYP, text="Funcionalidad 8  ", command="")
+        B8CYP.place(x=150, y=225)
+
+        B9CYP = tkinter.Button(CYP, text="Funcionalidad 9  ", command="")
+        B9CYP.place(x=150, y=250)
+
+        B10CYP = tkinter.Button(CYP, text="Funcionalidad 10", command="")
+        B10CYP.place(x=150, y=275)
+
+        B11CYP = tkinter.Button(CYP, text="Funcionalidad 11", command="")
+        B11CYP.place(x=150, y=300)
+
+        B12CYP = tkinter.Button(CYP, text="Funcionalidad 12", command="")
+        B12CYP.place(x=150, y=325)
+
+        B13CYP = tkinter.Button(CYP, text="Funcionalidad 13", command="")
+        B13CYP.place(x=150, y=350)
+
+        B14CYP= tkinter.Button(CYP, text="Funcionalidad 14", command="")
+        B14CYP.place(x=150, y=375)
+
+        B15CYP = tkinter.Button(CYP, text="Funcionalidad 15", command="")
+        B15CYP.place(x=150, y=400)
+
+        B16CYP = tkinter.Button(CYP, text="Funcionalidad 16", command=lambda: [])
+        B16CYP.place(x=150, y=425)
+
+        LogoEti = Label(CYP, text="Administra tu empresa").place(x=390, y=310)
+
+        self.ventana.withdraw()
+
+    def Proveedores(self):
+        Prov = Tk()
+        Prov.geometry("900x600")
+        Prov.title("Compras y Proveedores")
+
+
+#Contabilidad
+    def Contabilidad(self):
+        CONT = Tk()
+        CONT.geometry("900x600")
+        CONT.configure(bg="pink")
+        CONT.title("Contabilidad")
+
+        BCONT = tkinter.Button(CONT, text="Volver", command=lambda: [self.Atrás(CONT), ()])
+        BCONT.place(x=850, y=5)
+
+
+        ASYL = tkinter.Button(CONT, text="Stock Y \nLogística", height=5, width= 12, command=lambda: [self.StockYLogística(), CONT.withdraw()])
+        ASYL.place(x=30, y=100)
+
+        AVYC = tkinter.Button(CONT, text="Ventas Y \nClientes", height=5, width= 12, command=lambda: [self.VentasYClientes(), CONT.withdraw()])
+        AVYC.place(x=30, y=200)
+
+        ACYP = tkinter.Button(CONT, text="Compras Y \nProveedores", height=5, width= 12, command=lambda: [self.ComprasYProveedores(), CONT.withdraw()])
+        ACYP.place(x=30, y=300)
+
+        ACONT = tkinter.Button(CONT, text="Contabilidad", height=5, width=12, command=lambda: [])
+        ACONT.place(x=30, y=400)
+
+
+        B1CONT = tkinter.Button(CONT, text="Funcionalidad 1  ", command=lambda: [self.Fun1Contabilidad(),])
+        B1CONT.place(x=150, y=50)
+
+        B2CONT = tkinter.Button(CONT, text="Funcionalidad 2  ", command="")
+        B2CONT.place(x=150, y=75)
+
+        B3CONT = tkinter.Button(CONT, text="Funcionalidad 3  ", command="")
+        B3CONT.place(x=150, y=100)
+
+        B4CONT = tkinter.Button(CONT, text="Funcionalidad 4  ", command="")
+        B4CONT.place(x=150, y=125)
+
+        B5CONT = tkinter.Button(CONT, text="Funcionalidad 5  ", command="")
+        B5CONT.place(x=150, y=150)
+
+        B6CONT = tkinter.Button(CONT, text="Funcionalidad 6  ", command="")
+        B6CONT.place(x=150, y=175)
+
+        B7CONT = tkinter.Button(CONT, text="Funcionalidad 7  ", command="")
+        B7CONT.place(x=150, y=200)
+
+        B8CONT = tkinter.Button(CONT, text="Funcionalidad 8  ", command="")
+        B8CONT.place(x=150, y=225)
+
+        B9CONT = tkinter.Button(CONT, text="Funcionalidad 9  ", command="")
+        B9CONT.place(x=150, y=250)
+
+        B10CONT = tkinter.Button(CONT, text="Funcionalidad 10", command="")
+        B10CONT.place(x=150, y=275)
+
+        B11CONT = tkinter.Button(CONT, text="Funcionalidad 11", command="")
+        B11CONT.place(x=150, y=300)
+
+        B12CONT = tkinter.Button(CONT, text="Funcionalidad 12", command="")
+        B12CONT.place(x=150, y=325)
+
+        B13CONT = tkinter.Button(CONT, text="Funcionalidad 13", command="")
+        B13CONT.place(x=150, y=350)
+
+        B14CONT = tkinter.Button(CONT, text="Funcionalidad 14", command="")
+        B14CONT.place(x=150, y=375)
+
+        B15CONT = tkinter.Button(CONT, text="Funcionalidad 15", command="")
+        B15CONT.place(x=150, y=400)
+
+        B16CONT = tkinter.Button(CONT, text="Funcionalidad 16", command=lambda: [(), ()])
+        B16CONT.place(x=150, y=425)
+
+
+        self.ventana.withdraw()
+
+    def Fun1Contabilidad(self):
+        F1C = Tk()
+        F1C.geometry("900x600")
+        F1C.title("Función 1")
+
+
+#Main
+if __name__=='__main__':
+    warnings.filterwarnings('ignore')
+    Entorno_Grafico()
